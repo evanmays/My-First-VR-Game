e4a74fae395523ca32ade459df84d62959727a8b
Evan Mays <me@evanmays.com>
2018-01-06 20:42:39 -0500

    Enemies/friendlies functions to hide/show

--- index.js
+++ index.js
@@ -4,3 +4,35 @@ function showScoreboard() {
 function hideScoreboard() {
 	document.querySelector('#scoreboard').emit('hide');
 }
+
+function showEnemy(box) {
+	$("#box"+box+" a-plane.enemy").attr("visible", "true");
+}
+function showFriendly(box) {
+	$("#box"+box+" a-plane.friendly").attr("visible", "true");
+}
+
+function hideEnemy(box) {
+	$("#box"+box+" a-plane.enemy").attr("visible", "false");
+}
+
+function hideFriendly(box) {
+	$("#box"+box+" a-plane.friendly").attr("visible", "false");
+}
+
+life = false;
+$(window).keypress(function(e) {
+    if (e.which === 32) {
+    	if (life == false) {
+    		hideScoreboard();
+    		life = true;
+    		showEnemy(3);
+    	}
+        else {
+        	showScoreboard();
+        	life = false;
+        	hideEnemy(3);
+        }
+
+    }
+});
